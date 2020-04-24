a8413aba2cfe1b25241158d245a8a2dee5038898
Mahmoud Hassan <abdo.ra.1997@gmail.com>
2020-04-24 15:27:56 +0200

    first commit

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Stock"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Stocks"
+    }
+</script>
+
+<style scoped>
+
+</style>
